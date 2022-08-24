2735cd82fe368676397a4095ba1fd00478c20bc7
Yamilka <y.henriquez@tele.finance>
2022-08-24 10:49:23 -0400

    .

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    pt: 2,
+    px: 4,
+    pb: 3,
+};
+
+const Modal = () => {
+    const [open, setOpen] = React.useState(false);
+
+
+    return (
+        <>
+             {/* <span onClick={()=> setOpen(true);} data-bs-toggle="modal" data-bs-target="#exampleModal" className="p-card text-18 text-card">Reade more...</span>
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            ...
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+        </div>
+    </div>
+    </div> */}
+            </>
+    );
+}
+
+export default Modal
+
