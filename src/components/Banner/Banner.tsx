294a9a9ac5b0e814210c19faba83d290ac1f652f
yamilka <yamilka_268465460@hotmail.com>
2022-10-12 20:15:37 -0400

    add loading img

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -11,11 +11,11 @@ const BannerPage = () => {
                     <div className="hr-horizontal"></div>
                     <h1 className=" pt-5 header-m-5 mt-5 text-start">POKEDUX</h1>
                     <a href="#search-sections" className="header-m-5 align-items-center button-header d-flex  justify-content-center">
-                        <img alt="pokeball button" src="/assets/pokemonButton.svg" />
+                        <img alt="pokeball button" loading="eager" src="/assets/pokemonButton.svg" />
                         See pokemons
                     </a>
                     <div className="pokeball header-m-5  float-area">
-                        <img alt="pokeball" className="m-auto floating-img" src="/assets/pokeball.svg" />
+                        <img alt="pokeball" loading="eager" className="m-auto floating-img" src="/assets/pokeball.svg" />
                     </div>
                 </div>
                 <div className="header-rigth"></div>
